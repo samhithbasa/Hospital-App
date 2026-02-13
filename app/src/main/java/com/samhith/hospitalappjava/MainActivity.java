c76095655054924e9180efb05b64fff2d0253969
samhithbasa <samhithbasa12@gmail.com>
2026-02-13 19:03:02 +0530

    Update APK with secondary enhancements and fix build issues

--- app/src/main/java/com/samhith/hospitalappjava/MainActivity.java
+++ app/src/main/java/com/samhith/hospitalappjava/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import com.google.firebase.auth.FirebaseAuth;
